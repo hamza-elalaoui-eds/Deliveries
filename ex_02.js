fe683d5521e42590a95e91fd38d5ba1f451a9397
hamza.elalaoui <hamza.elalaoui@plexus.tech>
2024-10-05 17:26:26 +0100

    Add ex_02.js with variable declarations and display using displayThisText()

---
+++ ex_02.js
@@ -0,0 +1,11 @@
+var bool  = true;
+var string = "forty two";
+var integer = 42;
+var float = 42.42;
+var this_is_null = null;
+
+displayThisText(bool);
+displayThisText(string);
+displayThisText(integer);
+displayThisText(float);
+displayThisText(this_is_null);

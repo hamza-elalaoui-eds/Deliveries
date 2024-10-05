8851598f41ef91abea1c5688b221be658b3d528b
hamza.elalaoui <hamza.elalaoui@plexus.tech>
2024-10-05 17:11:49 +0100

    reset branch

--- ex_04.js
+++
@@ -1,5 +0,0 @@
-// Displaying the message on the result page
-document.body.innerHTML = "Welcome to the JavaScript pool! Have some fun and dont forget to bring a towel.";
-
-// Logging message in the console
-console.log("Website launched!");

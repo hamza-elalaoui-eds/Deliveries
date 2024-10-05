19cb4d961f610f15d69036c3c3d1b31fcdeee6e5
hamza.elalaoui <hamza.elalaoui@plexus.tech>
2024-10-05 23:10:49 +0100

    Rearranged lines in changeBackground() to correctly apply color changes to rectangles

---
+++ ex_03.js
@@ -0,0 +1,17 @@
+function changebackground() {
+    let magicNumber = 1;
+  
+    if (magicNumber === 1) {
+      paintYellowRectangle();
+    }
+    
+    magicNumber = 2;
+    if (magicNumber === 2) {
+      paintGreenRectangle();
+    }
+    
+    magicNumber = 3;
+    if (magicNumber === 3) {
+      paintRedRectangle();
+    }
+  }

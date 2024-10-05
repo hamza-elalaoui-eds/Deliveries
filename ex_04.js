8f8642d4cffde428bbdadd59092928d5094b83ad
hamza.elalaoui <hamza.elalaoui@plexus.tech>
2024-10-05 16:37:00 +0100

    Add ex_04.js for message display and console log

---
+++ ex_04.js
@@ -0,0 +1,5 @@
+// Displaying the message on the result page
+document.body.innerHTML = "Welcome to the JavaScript pool! Have some fun and dont forget to bring a towel.";
+
+// Logging message in the console
+console.log("Website launched!");

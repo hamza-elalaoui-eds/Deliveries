430a2e450e2111c3b21243fd6f22f5f68685ad05
hamza.elalaoui <hamza.elalaoui@plexus.tech>
2024-10-05 17:41:14 +0100

    Add ex_04.js to demonstrate typeof operator

---
+++ ex_04.js
@@ -0,0 +1,15 @@
+var undefinedVar;
+var booleanVar = true;
+var numberVar = 1;
+var bigintVar = BigInt(1);
+var symbolVar = Symbol('unique');
+var objectVar = {};
+var functionVar = function() {};
+
+displayThisText(typeof undefinedVar);
+displayThisText(typeof booleanVar);
+displayThisText(typeof numberVar);
+displayThisText(typeof bigintVar);
+displayThisText(typeof symbolVar);
+displayThisText(typeof objectVar);
+displayThisText(typeof functionVar);

2d4f2a1d43d32868563c53c1c97ccd8d4885831d
hamza.elalaoui <hamza.elalaoui@plexus.tech>
2024-10-06 12:42:25 +0100

    Implement getBasketContent() to return list of remaining fruits and diplay the total count

---
+++ ex_05.js
@@ -0,0 +1,13 @@
+function getBasketContent(){
+	const fruits = [
+  	'strawberry','strawberry','strawberry',
+    'apple','apple',
+    'lime','lime',
+    'peach',
+    'pear','pear'
+   ];
+   
+   console.log(`${fruits.length} fruit(s) selected`);
+   
+   return fruits;
+}

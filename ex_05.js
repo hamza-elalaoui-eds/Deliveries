22acdf9af3fcb2b6597da39a0dca77fdfa61ab8c
Hamza Elalaoui <hamza.el-alaoui@epitech.digital>
2024-10-06 14:41:13 +0100

    Implement getHousesPrototype() to return a map of apartments and their areas

---
+++ ex_05.js
@@ -0,0 +1,10 @@
+function getHousesPrototype() {
+    const houses = new Map();
+    
+    houses.set('Apartment 75008', 90);
+    houses.set('Apartment 94300', 105);
+    houses.set('Apartment 92300', 140);
+    houses.set('Apartment 75015', 65);
+
+    return houses;
+}
